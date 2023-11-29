a503b08562b145d6c92823fec042ba3234a78948
daniel <danieloliveira.s558@gmail.com>
2023-11-29 10:59:16 -0300

    Verificando lista de dados dos medicos

--- GerenciamentoConsultorioMedico/InterfaceUsuario.cs
+++ GerenciamentoConsultorioMedico/InterfaceUsuario.cs
@@ -1,6 +1,21 @@
-
+namespace GerenciamentoConsultorioMedico;
 class InterfaceUsuario{
 
 
+    public static void Main(){
+
+        Medicos medicos = new Medicos("Daniel", DateTime.Parse("2000-01-01"), "12345678910", "123456789");
+        exibirMedicos(medicos);
+    }
+
+    public static void exibirMedicos( Medicos medicos) {
     
-}
+        System.Console.WriteLine("Lista de Medicos: ");
+        foreach (var item in medicos.getMedicos())
+        {
+            
+            System.Console.WriteLine($"{item.nome} - {item.dataNascimento} - {item.cpf} - {item.crm}");
+        }
+    }
+ }
+    
--- GerenciamentoConsultorioMedico/Medico.cs
+++ GerenciamentoConsultorioMedico/Medico.cs
@@ -1,5 +1,24 @@
 using System;
-class Medicos: Pessoa {
-    List<(string nome, DataTime dataNacimento, string cpf, string crm )> listaDeMedicos
+using System.Collections.Generic;
 
+class Medicos : Pessoa {
+    string crm;
+    List<(string nome, DateTime dataNascimento, string cpf, string crm)> listaDeMedicos;
+
+    public Medicos(string nome, DateTime dataNascimento, string cpf, string crm) {
+        this.nome = nome;
+        this.dataNascimento = dataNascimento;
+        this.cpf = cpf;
+        this.crm = crm;
+
+        listaDeMedicos = new List<(string nome, DateTime dataNascimento, string cpf, string crm)>();
+        // Adicionar valores predefinidos à lista
+        
+    }
+    public bool adicionarPaciente() {
+        listaDeMedicos.Add((this.nome, this.dataNascimento, this.cpf, this.crm));
+    }
+    public List<(string nome, DateTime dataNascimento, string cpf, string crm)> getMedicos() {
+        return listaDeMedicos;
+    }
 }
